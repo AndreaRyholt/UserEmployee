3ea176243eea7fadd76e4d8a884e7a072077f42f
AndreaRyholt <anra0003@stud.kea.dk>
2024-09-05 13:34:51 +0200

    Added PostMapping for user and employee

--- src/main/java/com/example/useremployee/controller/EmployeeRestController.java
+++ src/main/java/com/example/useremployee/controller/EmployeeRestController.java
@@ -1,10 +1,12 @@
 package com.example.useremployee.controller;
 
 import com.example.useremployee.model.Employee;
+import com.example.useremployee.model.User;
 import com.example.useremployee.repositories.EmployeeRepository;
+import com.example.useremployee.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,9 +16,26 @@ public class EmployeeRestController {
     @Autowired
     EmployeeRepository employeeRepository;
 
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping("/employees")
     public List<Employee> getusers() {
         return employeeRepository.findAll();
+    }
+
+    @PostMapping("/user")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User postUser(@RequestBody User user) {
+        System.out.println(user);
+        return userRepository.save(user);
+    }
+
+    @PostMapping("/employee")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Employee postEmployee(@RequestBody Employee employee) {
+        System.out.println(employee);
+        return employeeRepository.save(employee);
     }
 
 
